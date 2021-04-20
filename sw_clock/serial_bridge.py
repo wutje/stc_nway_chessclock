50edf416d9bdadbe4d66fae3bd0f9d0abf54ba36
Yuri Schaeffer <yuri@schaeffer.tk>
2021-04-20 23:55:00 +0200

    accumulate messages and display

--- sw_clock/serial_bridge.py
+++ sw_clock/serial_bridge.py
@@ -10,6 +10,8 @@ PORT = sys.argv[1]
 FN_IN = sys.argv[2]
 FN_OUT = sys.argv[3]
 
+SYNC_BYTE = b's' ## from uart.c
+MSG_LEN = 5 ## inferred from uart.c
 
 print(f"Opening {FN_IN} for reading")
 PIPEIN = aiofiles.open(FN_IN, 'rb')
@@ -18,8 +20,25 @@ PIPEOUT = open(FN_OUT, 'wb')
 
 snooper = None;
 
+class Accumulator:
+    def __init__(self, msglen, name):
+        self.msglen = msglen
+        self.i = 0;
+        self.bytes = [0]*msglen
+        self.name = name
+    def add(self, b):
+        self.bytes[self.i] = b
+        if self.i != 0 or b == SYNC_BYTE:
+            self.i += 1
+        if self.i == self.msglen:
+            msg = b''.join(self.bytes)
+            hx = msg.hex(' ')
+            sys.stderr.write(f"{self.name}: {msg} ({hx})\n")
+            self.i = 0
+
 async def read_pipe():
     async with PIPEIN as f:
+        accu = Accumulator(msglen=MSG_LEN, name = 'Pipe in')
         while True:
             try:
                 b = await f.read(1)
@@ -29,6 +48,7 @@ async def read_pipe():
             if len(b) == 0:
                 print("EOF")
                 return
+            accu.add(b)
             print(f"PIPEIN {b}")
             snooper.write(b)
 
@@ -36,6 +56,7 @@ class SerialSnoop(asyncio.Protocol):
     def __init__(self, pipe_task):
         super().__init__()
         self.pipe_task = pipe_task
+        self.accu = Accumulator(msglen=MSG_LEN, name = 'Serial in')
 
     def connection_made(self, transport):
         global snooper
@@ -45,6 +66,7 @@ class SerialSnoop(asyncio.Protocol):
 
     def data_received(self, data):
         print(f"SERIALIN {data}")
+        self.accu.add(data)
         try:
             PIPEOUT.write(data)
             PIPEOUT.flush()
